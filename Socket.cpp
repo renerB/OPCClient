65e929429e7f1a611b15e236df49a064b3c408d4
Brener Chaves <brenerchaves@hotmail.com>
2022-11-01 20:16:28 -0300

    Update threads

--- Socket.cpp
+++ Socket.cpp
@@ -26,32 +26,64 @@ using namespace std;
 mutex SeqMtx;
 mutex ReadMtx;
 
-void sendDados_thread(SOCKET sock, char buf[50], int* nSeq, float* TempPreAq, float* TempAq, float* TempEnch, float* Vazao, char msgDados[TamMsgDados + 1], char msgAck[TamAck + 1], int* status) {
-
-	// Envio de mensagem:
-	memset(msgDados, 0, TamMsgDados + 1);
-	SeqMtx.lock();
-	ReadMtx.lock();
-	sprintf(msgDados, "%06d;0000;%05.1f;%05.1f;%05.1f;%05.1f", *nSeq, *TempPreAq, *TempAq, *TempEnch, *Vazao);
-	ReadMtx.unlock();
-	*status = send(sock, msgDados, TamMsgDados, 0);
-	// Falta msg de erro
-
-// Recebimento de confirmação:
-	memset(msgAck, 0, TamAck + 1);
-	*status = recv(sock, msgAck, TamAck, 0);
-	// Falta msg de erro
-	buf = strtok(msgAck, ";");
-	// Falta msg de erro e confrmação dos bits do Ack
-	if (atoi(buf) != ((*nSeq) + 1)) {
-		printf("Erro de sequenciamento - ACK");
+void SetupSocket(char* ipaddr) {
+	// Variáveis de conexão:
+	WSADATA wsaData;
+	SOCKET sock;
+	SOCKADDR_IN ServerAddr;
+	int status;
+	// Criação de novo socket para estabelecer conexão:
+	sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == INVALID_SOCKET) {
+		status = WSAGetLastError();
+		if (status == WSAENETDOWN)
+			printf("Rede ou servidor de sockets inacessíveis!\n");
+		else
+			printf("Falha na rede: codigo de erro = %d\n", status);
+		WSACleanup();
+		exit(0);
 	}
-	*nSeq = atoi(buf) + 1;
-	SeqMtx.unlock();
 
-	// Controle de temporiação de mensagem:
-	this_thread::sleep_for(chrono::milliseconds(2000));
+	// Inicializa a estrutura SOCKADDR_IN que será utilizada para
+	// a conexão ao servidor.
+	ServerAddr.sin_family = AF_INET;
+	ServerAddr.sin_port = htons(port);
+	ServerAddr.sin_addr.s_addr = inet_addr(ipaddr);
 
+	// Estabelece a conexão com o servidor
+	status = connect(sock, (SOCKADDR*)&ServerAddr, sizeof(ServerAddr));
+	if (status == SOCKET_ERROR) {
+		printf("Falha na conexao ao servidor ! Erro  = %d\n", WSAGetLastError());
+		WSACleanup();
+		exit(0);
+	}
+}
+
+void sendDados_thread(SOCKET sock, char buf[50], int* nSeq, float* TempPreAq, float* TempAq, float* TempEnch, float* Vazao, char msgDados[TamMsgDados + 1], char msgAck[TamAck + 1], int* status) {
+	while (true) {
+		memset(msgDados, 0, TamMsgDados + 1);
+		SeqMtx.lock();
+		ReadMtx.lock();
+		sprintf(msgDados, "%06d;0000;%05.1f;%05.1f;%05.1f;%05.1f", *nSeq, *TempPreAq, *TempAq, *TempEnch, *Vazao);
+		ReadMtx.unlock();
+		*status = send(sock, msgDados, TamMsgDados, 0);
+		// Falta msg de erro
+
+	// Recebimento de confirmação:
+		memset(msgAck, 0, TamAck + 1);
+		*status = recv(sock, msgAck, TamAck, 0);
+		// Falta msg de erro
+		buf = strtok(msgAck, ";");
+		// Falta msg de erro e confrmação dos bits do Ack
+		if (atoi(buf) != ((*nSeq) + 1)) {
+			printf("Erro de sequenciamento - ACK");
+		}
+		*nSeq = atoi(buf) + 1;
+		SeqMtx.unlock();
+
+		// Controle de temporiação de mensagem:
+		this_thread::sleep_for(chrono::milliseconds(2000));
+	}
 }
 
 void SocketMainThread(char* ipaddr, float* TempPreAq, float* TempAq, float* TempEnch, float* Vazao, float* SetTempPreAq, float* SetTempAq, int* SetTempEnch, int* key) {
@@ -105,14 +137,9 @@ void SocketMainThread(char* ipaddr, float* TempPreAq, float* TempAq, float* Temp
 		exit(0);
 	}
 
+	std::thread t1(sendDados_thread, sock, buf, &nSeq, TempPreAq, TempAq, TempEnch, Vazao, msgDados, msgAck, &status);
+
 	while(true) {
-
-		// Criação de Thread de envio sincrono:
-		sendDados_thread(sock, buf, &nSeq, TempPreAq, TempAq, TempEnch, Vazao, msgDados, msgAck, &status);
-
-		// Sincronização de threads:
-		//sendDados.join(); // Sistema interrompe para realização de thread
-
 		if (*key == 1) {
 			memset(msgReqSet, 0, TamReqSet + 1);
 			SeqMtx.lock();
